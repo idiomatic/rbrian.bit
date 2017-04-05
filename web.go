0dba5a33cf41437da8697c8693f1c84d9152b789
brian harrison <brian@idiomatic.org>
2017-04-05 12:48:22 -0700

    adds QR code

--- web.go
+++ web.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
 func init() {
-	//http.Handle("/", http.FileServer(http.Dir("static")))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello explorer.\n")
-	})
+	http.Handle("/", http.FileServer(http.Dir("static")))
 }
 
 func main() {
